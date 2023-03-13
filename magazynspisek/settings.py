c511686a7ce9d3e80d4a119c16fef337617a2ed2
wiaderko024 <kajtek.maslak@icloud.com>
2023-03-13 18:54:19 +0100

    Imported SASS theme. Created pages django application. Created base_index.html and basic view of home page.

--- magazynspisek/settings.py
+++ magazynspisek/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'authors'
+    'authors',
+    'pages'
 ]
 
 MIDDLEWARE = [
@@ -122,6 +123,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = 'static/'
+
+MEDIA_URL = 'media/'
+MEDIA_ROOT = 'media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
