a1f5c5c7954d7fb8872fc357550c62085858be8a
wiaderko024 <kajtek.maslak@icloud.com>
2023-03-13 23:00:21 +0100

    Created basic author details page

--- authors/urls.py
+++ authors/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import authors_list
+from .views import authors_list, authors_details
 
 urlpatterns = [
-    path('', authors_list, name='authors-list')
+    path('', authors_list, name='authors-list'),
+    path('<int:pk>/', authors_details, name='authors-details')
 ]
--- authors/views.py
+++ authors/views.py
@@ -6,3 +6,8 @@ from .models import Author
 def authors_list(request):
     authors = Author.objects.all()
     return render(request, 'authors_list.html', {'authors': authors})
+
+
+def authors_details(request, pk):
+    author = Author.objects.get(pk=pk)
+    return render(request, 'authors_details.html', {'author': author})
