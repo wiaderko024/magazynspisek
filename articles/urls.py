955c87b6d1091e620e1117fb8c6050505b6fcf0a
wiaderko024 <kajtek.maslak@icloud.com>
2023-03-14 23:49:09 +0100

    Created article detail p[age

--- articles/urls.py
+++ articles/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import articles_list
+from .views import articles_list, articles_details
 
 urlpatterns = [
-    path('', articles_list, name='articles-list')
+    path('', articles_list, name='articles-list'),
+    path('<int:pk>/', articles_details, name='articles-details')
 ]
--- articles/views.py
+++ articles/views.py
@@ -3,5 +3,10 @@ from .models import Article
 
 
 def articles_list(request):
-    articles = Article.objects.all().order_by('date')
+    articles = Article.objects.all().order_by('-date')
     return render(request, 'articles_list.html', {'articles': articles})
+
+
+def articles_details(request, pk):
+    article = Article.objects.get(pk=pk)
+    return render(request, 'articles_details.html', {'article': article})
