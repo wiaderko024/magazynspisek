d856724c2690e77ec54b228b77778586629cab40
wiaderko024 <kajtek.maslak@icloud.com>
2023-07-24 16:56:09 +0200

    home page

--- pages/views.py
+++ pages/views.py
@@ -1,5 +1,29 @@
 from django.shortcuts import render
 
+from articles.models import Article
+
 
 def home_page(request):
-    return render(request, 'index.html')
+    article = Article.objects.get(main_article=True)
+
+    if article.long_cover_theme == 'dark':
+        styles = {
+            'logo_color': '#EDEADE',
+            'archive_color': '#EDEADE',
+            'h1_color': '#EDEADE',
+            'color': '#EDEADE'
+        }
+    else:
+        styles = {
+            'logo_color': '#323d47',
+            'archive_color': '#EDEADE',
+            'h1_color': '#323d47',
+            'color': '#fff'
+        }
+
+    context = {
+        'article': article,
+        'styles': styles
+    }
+
+    return render(request, 'index.html', context)
