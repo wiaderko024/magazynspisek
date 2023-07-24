df7344e9987313b7ecfb128c0e1ec09d4ea6496e
wiaderko024 <kajtek.maslak@icloud.com>
2023-07-24 15:16:05 +0200

    Long article cover model fields

--- articles/models.py
+++ articles/models.py
@@ -16,6 +16,22 @@ class Article(models.Model):
         blank=True,
         null=True
     )
+
+    main_article = models.BooleanField(default=False, blank=False, null=False)
+    long_cover = models.ImageField(upload_to='articles_long_covers', blank=True, null=True)
+    long_cover_author = models.ForeignKey(
+        Author,
+        on_delete=models.DO_NOTHING,
+        related_name='long_cover_author',
+        blank=True,
+        null=True
+    )
+    LONG_COVER_THEMES = [
+        ('white', 'white'),
+        ('dark', 'dark')
+    ]
+    long_cover_theme = models.CharField(max_length=5, choices=LONG_COVER_THEMES, blank=True, null=True)
+
     date = models.DateField(blank=True, null=True)
     article = RichTextField(blank=False, null=False)
     ARTICLE_TYPES = [
@@ -28,6 +44,14 @@ class Article(models.Model):
     author = models.ForeignKey(Author, on_delete=models.DO_NOTHING, blank=False, null=False)
     created_at = models.DateTimeField(auto_now_add=True, editable=False, null=False)
     updated_at = models.DateTimeField(auto_now=True, editable=False, null=True)
+
+    def save(self, *args, **kwargs):
+        if self.main_article:
+            for article in Article.objects.filter(main_article=True):
+                article.main_article = False
+                article.save()
+
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return self.title
